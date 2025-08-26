0aca82418260cffa30f47bd2e9d4c233bd74318a
choidh88 <dev.cdh88@gmail.com>
2025-08-26 19:40:50 +0900

    이미지 추가 및 이미지 슬라이드 기능 구현

--- main.js
+++ main.js
@@ -1,45 +1,65 @@
 "use strict";
 
+const data = {
+  box: 1,
+  lying: 3,
+  nosnowwinter: 2,
+  rolling: 5,
+  snake: 4,
+  sun: 5,
+  water: 3,
+  winter: 4,
+  yawning: 2,
+};
+
+let slideInterval = 0;
+
 function show(dirName) {
+  // 로그 출력
+  const imageTotalNumber = data[dirName];
+  console.log(
+    `User clicked: '${dirName}', total number of images: ${imageTotalNumber}`
+  );
+
+  // 슬라이드 이미지 초기화 및 채우기
   const slide = document.getElementsByClassName("Slidesbackground")[0];
   slide.innerHTML = "";
 
-  // 이거 아이템화하기
-  const item = document.createElement("div");
-  item.classList = ["mySlide fade"];
+  for (let i = 0; i < imageTotalNumber; i++) {
+    const item = document.createElement("div");
+    item.classList = ["mySlide fade"];
 
-  const img = document.createElement("img");
-  img.classList = ["slideshow-image"];
-  img.src = `./image/${dirName}/1.jpg`;
+    const img = document.createElement("img");
+    img.classList = ["slideshow-image"];
+    img.src = `./image/${dirName}/${i + 1}.jpg`;
 
-  item.appendChild(img);
-  slide.appendChild(item);
-  // --
+    item.appendChild(img);
+    slide.appendChild(item);
+  }
+
+  // 슬라이드 시작
+  let slides = document.getElementsByClassName("mySlide");
+  for (let i = 1; i < slides.length; i++) slides[i].style.display = "none";
+  console.log(`현재 슬라이드: 1 / ${imageTotalNumber}`);
+
+  let slideNum = 0;
+  slideInterval = setInterval(() => {
+    slides[slideNum].style.display = "none";
+
+    slideNum++;
+    slideNum =
+      ((slideNum % imageTotalNumber) + imageTotalNumber) % imageTotalNumber; // 항상 0~imageTotalNumber 유지
+    console.log(`현재 슬라이드: ${slideNum + 1} / ${imageTotalNumber}`);
+
+    slides[slideNum].style.display = "block";
+  }, 3000); // 일정 시간(ms)마다 변경
 
   // 팝업창 활성화
   document.getElementById("popup").style.display = "";
 }
 
 function hide() {
+  clearInterval(slideInterval);
   // 팝업창 비활성화
   document.getElementById("popup").style.display = "none";
 }
-
-// var slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {
-//     slideIndex = 1;
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-
-//   setTimeout(showSlides, 2000); // 2초마다 이미지가 체인지됩니다
-// }
